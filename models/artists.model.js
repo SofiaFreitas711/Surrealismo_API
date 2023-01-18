a26e944e56c73448429042d83fec0a2e88828a55
Sofia Freitas <anasofiafreitas711@gmail.com>
2023-01-18 17:12:06 +0000

    date to string

--- models/artists.model.js
+++ models/artists.model.js
@@ -5,8 +5,8 @@ const schema = new mongoose.Schema(
         name: { type: String, required: [true, 'Falta o nome']},
         image: { type: String, required: [true, 'Falta a imagem']},
         info: { type: String, required: [true, 'Falta uma descrição']},
-        born:{type: Date, required: [true, 'Falta a data de nascimento']},
-        death:{type: Date},
+        born:{type: String, required: [true, 'Falta a data de nascimento']},
+        death:{type: String},
         arts: {type: Array}
     }
 );
